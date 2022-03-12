6e14b5e95a885921911ed0f4914dbe6dae9ed438
Aleksey Zarudny <zarudnyy2@gmail.com>
2022-03-12 20:43:31 +0300

    second screen

--- MyProfile/ViewControllers/WelcomeViewController.swift
+++ MyProfile/ViewControllers/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  MyProfile
 //
 //  Created by Алексей Зарудный on 12.03.2022.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
 
 }
-
