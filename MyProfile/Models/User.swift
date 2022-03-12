84ebcb7972542b47c98e693a74f9ecf6f17fc131
Aleksey Zarudny <zarudnyy2@gmail.com>
2022-03-13 01:24:59 +0300

    model

---
+++ MyProfile/Models/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  MyProfile
+//
+//  Created by Алексей Зарудный on 13.03.2022.
+//
+
+struct User {
+    let username: String
+    let password: String
+    let person: Person
+}
+
+struct Person {
+    let name: String
+    let surname: String
+    let age: Int
+    let job: String
+    let hobby: String    
+}
+
